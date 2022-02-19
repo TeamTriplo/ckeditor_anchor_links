61ef937515c1cab42aac99d65bc4f3466fadd5a1
Mahmudul Hasan <mahmudul69@hotmail.com>
2022-02-20 02:35:20 +0600

    trying to get the button

--- anchor_link/plugins/anchor_link/lang/en.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-CKEDITOR.plugins.setLang( 'anchor_link', 'en', {
-  block: 'Anchor Link',
-  blockTitle: 'Anchor Link'
-} );
--- plugins/ckeditor_anchor_links/plugin.js
+++ plugins/ckeditor_anchor_links/plugin.js
@@ -1,29 +1,26 @@
-alert('test');
-
-CKEDITOR.plugins.add( 'anchor_link', {
+CKEDITOR.plugins.add( 'ckeditor_anchor_links', {
 
     // Register the icons. They must match command names.
-    icons: 'anchor_link',
+    icons: 'ckeditor_anchor_links',
 
     // The plugin initialization logic goes inside this method.
     init: function( editor ) {
-
         // Define the editor command that inserts a anchor_link.
-        editor.addCommand( 'insertanchor_link', {
+        editor.addCommand( 'ckeditor_anchor_links', {
 
             // Define the function that will be fired when the command is executed.
             exec: function( editor ) {
                 var now = new Date();
-
+                console.log('here');
                 // Insert the anchor_link into the document.
                 editor.insertHtml( 'The current date and time is: <em>' + now.toString() + '</em>' );
             }
         });
 
         // Create the toolbar button that executes the above command.
-        editor.ui.addButton( 'anchor_link', {
+        editor.ui.addButton( 'CkeditorAnchorLinks', {
             label: 'Insert anchor_link',
-            command: 'insertanchor_link',
+            command: 'ckeditor_anchor_links',
             toolbar: 'insert'
         });
     }
